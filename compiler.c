d5a42252becc4f1fe703dee83d93972e5894228a
Khiem Hai <hnbandomachi@gmail.com>
2019-09-09 23:53:17 +0700

    add exe

---
+++ compiler.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main() {
+int x;
+int y;
+int z;
+int a;
+int b;
+int c;
+int d;
+printf("Enter x ="); scanf("%d", &x);
+printf("Enter y ="); scanf("%d", &y);
+printf("Enter z ="); scanf("%d", &z);
+a = x + y;
+b = y - z;
+c = x * z;
+d = a / b;
+printf("Result a = %d", a);
+printf("Result b = %d", b);
+printf("Result c = %d", c);
+printf("Result d = %d", d);
+return 0;
+}
