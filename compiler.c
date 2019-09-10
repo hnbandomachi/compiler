58a9e67b748cd75a0e827ace2b48930da08d036e
Khiem Hai <hnbandomachi@gmail.com>
2019-09-10 22:34:03 +0700

    format text python

--- compiler.c
+++ compiler.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main() {
 int x;
 int y;
@@ -7,16 +8,16 @@ int a;
 int b;
 int c;
 int d;
-printf("Enter x ="); scanf("%d", &x);
-printf("Enter y ="); scanf("%d", &y);
-printf("Enter z ="); scanf("%d", &z);
+printf("Enter x = "); scanf("%d", &x);
+printf("Enter y = "); scanf("%d", &y);
+printf("Enter z = "); scanf("%d", &z);
 a = x + y;
 b = y - z;
 c = x * z;
 d = a / b;
-printf("Result a = %d", a);
-printf("Result b = %d", b);
-printf("Result c = %d", c);
-printf("Result d = %d", d);
-return 0;
+printf("Result a = %d ", a );
+printf("Result b = %d ", b );
+printf("Result c = %d ", c );
+printf("Result d = %d ", d );
+system("pause");
 }
--- compiler.py
+++ compiler.py
@@ -1,47 +1,51 @@
+declarations = []
+lines = []
+
 def notExit(word):
-    for i in range(len(declareCode)):
-        if word == declareCode[i]:
+    for i in range(len(declarations)):
+        if word == declarations[i]:
             return False
     return True
 
-lines = []
-codeC = "#include<stdio.h>\nint main() {\n"
-declareCode = []
-with open('add.txt') as my_file:
-    for line in my_file:
-        lines.append(line)
+def main():
+    codeC = "#include<stdio.h>\n#include<stdlib.h>\nint main() {\n"
+    with open('add.txt') as my_file:
+        for line in my_file:
+            lines.append(line)
 
-numLines = len(lines)
-for i in range(numLines):
-    words = lines[i].replace('\n', '').split(" ")
-
-    for j in range(1, len(words)):
-        if notExit(words[j]):
-            codeC = codeC + 'int ' + words[j] + ';\n'
-            declareCode.append(words[j])
-
-for i in range(len(lines)):
-    words = lines[i].replace('\n', '').split(" ")
-    if words[0] == "INPUT":
+    for i in range(len(lines)):
+        words = lines[i].replace('\n', '').split(" ")
         for j in range(1, len(words)):
-            codeC = codeC + 'printf("Enter ' + words[j] + ' ="; scanf("%d", &' + words[j] + ');\n'
+            if notExit(words[j]):
+                codeC = codeC + 'int ' + words[j] + ';\n'
+                declarations.append(words[j])
 
-    if words[0] == "ADD":
+    for i in range(len(lines)):
+        words = lines[i].replace('\n', '').split(" ")
+        if words[0] == "INPUT":
+            for j in range(1, len(words)):
+                codeC = codeC + 'printf("Enter ' + words[j] + ' = "); scanf("%d", &' + words[j] + ');\n'
+
+        if words[0] == "ADD":
             codeC = codeC + words[1] + ' = ' + words[2] + ' + ' + words[3] + ';\n'
 
-    if words[0] == "SUB":
+        if words[0] == "SUB":
             codeC = codeC + words[1] + ' = ' + words[2] + ' - ' + words[3] + ';\n'
 
-    if words[0] == "MUL":
+        if words[0] == "MUL":
             codeC = codeC + words[1] + ' = ' + words[2] + ' * ' + words[3] + ';\n'
 
-    if words[0] == "DIV":
+        if words[0] == "DIV":
             codeC = codeC + words[1] + ' = ' + words[2] + ' / ' + words[3] + ';\n'
 
-for i in range(1, len(lines) - 1):
-    words = lines[i].replace('\n', '').split(" ")
-    codeC = codeC + 'printf("Result ' + words[1] + ' = %d, ' + words[1] + ');\n'
+    for i in range(1, len(lines) - 1):
+        words = lines[i].replace('\n', '').split(" ")
+        codeC = codeC + 'printf("Result ' + words[1] + ' = %d ", ' + words[1] + ' );\n'
 
-codeC = codeC + 'return 0;' + '\n}'
+    codeC = codeC + 'system("pause");\n}'
 
-print(codeC)
+    fileC = open("compiler.c", "w+")
+    fileC.write(codeC)
+
+if __name__ == "__main__":
+    main()
